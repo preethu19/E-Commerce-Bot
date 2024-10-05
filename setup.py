05a0ccb094201130408a79016871cda34abc9801
preethu19 <preethamkumar2000@gmail.com>
2024-10-05 23:48:09 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Ecommerce Bot",
+    version= '0.0.0',
+    author="Preetham",
+    author_email="preetham@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
